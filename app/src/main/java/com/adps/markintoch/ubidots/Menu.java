deb49338150a997e2d027e5cdfedcc37360d2b1e
Luis Marquez <Luis Marquez>
2017-05-02 00:39:11 -0500

    Revert "Revert "Agregada la visualizacion de datos de todas las variables mediante el uso de la api de Ubidots""
    This reverts commit 62503b6abb4e26f23eb86cd6828405e5b9f1c3d9.

---
+++ app/src/main/java/com/adps/markintoch/ubidots/Menu.java
@@ -0,0 +1,49 @@
+package com.adps.markintoch.ubidots;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class Menu extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        findViewById(R.id.Monitoreo).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(Menu.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+        findViewById(R.id.Hum).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(Menu.this, Humedad.class);
+                startActivity(i);
+            }
+        });
+        findViewById(R.id.Temp).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(Menu.this, Temperatura.class);
+                startActivity(i);
+            }
+        });
+        findViewById(R.id.Guia).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(Menu.this, GuiaRapida.class);
+                startActivity(i);
+            }
+        });
+    }
+
+}
